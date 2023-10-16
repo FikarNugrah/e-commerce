3a396db771630aca164962c24e47ffc0a7df6045
yafikar nugrah paskah <88127609+FikarNugrah@users.noreply.github.com>
2023-10-16 09:09:28 +0700

    edit: position rate

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -78,10 +78,7 @@ export default function Products() {
               <p className="categories">{product.category}</p>
               <p className="rate">
                 <i className="fa-solid fa-star"></i>
-                <span style={{ height: "19px" }}>
-                  {product.rating.rate}
-                </span>{" "}
-                <span className="or">|</span>{" "}
+                <span>{product.rating.rate}</span> <span className="or">|</span>{" "}
                 <span className="count">{product.rating.count} Rv</span>
               </p>
               <div className="price-area">
